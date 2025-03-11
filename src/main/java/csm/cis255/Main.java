99836aa6c87c50f927af90ddb3d2d819264a8bc5
theMaximmus <m.stesev@gmail.com>
2025-03-10 21:12:13 -0700

    Second part of Jasmine's commit. Need to fix Driver.java as it does not actually drop the student. The problem might be that we create a copy of a real student with no name, and so it does not match the existing student, which in the end leaves them in the roster

--- src/main/java/csm/cis255/Main.java
+++ src/main/java/csm/cis255/Main.java
@@ -2,7 +2,5 @@ package csm.cis255;
 
 public class Main {
     public static void main(String[] args) {
-        Course course = new Course("COUN110", 10, 5);
-        System.out.println(course);
     }
 }
